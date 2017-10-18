5a8819091cd1ab1b51c609e8ce3cf2dec4836c78
Felix Krull <f_krull@gmx.de>
2017-10-18 23:15:05 +0200

    Some more attempts at async helpers.

--- src/extension/async.cljs
+++ src/extension/async.cljs
@@ -2,6 +2,12 @@
 
 (defn waterfall-internal
     ([values callback]
-     (apply callback values)))
+     (apply callback values))
+    ([values callback & callbacks]
+     ;(println values)
+     ;(println callback)
+     ;(println callbacks)
+     ;(println (conj values (fn [& values] (apply waterfall-internal (cons values callbacks)))))
+     (apply callback (conj values (fn [& values] (apply waterfall-internal (cons (vec values) callbacks)))))))
 
 (defn waterfall [& callbacks] (apply waterfall-internal (cons [] callbacks)))
--- src/test/test_async.cljs
+++ src/test/test_async.cljs
@@ -7,13 +7,21 @@
     (async/timeout 0 done
         (extension.async/waterfall done)))
 
-; (deftest waterfall-should-call-functions-in-seq
-;     (async test-done
-;         (extension.async/waterfall
-;             (fn [done] (done nil 1))
-;             (fn [arg done] (done nil (+ arg 1)))
-;             (fn [arg done] (done nil (+ arg 1)))
-;             (fn [arg done]
-;                 (is (= arg 3))
-;                 (done))
-;             test-done)))
+(deftest waterfall-should-call-two-functions-in-sequence
+    (async/timeout 0 all-done
+        (extension.async/waterfall
+            (fn [done] (done "some value"))
+            (fn [some-value]
+                (is (= some-value "some value"))
+                (all-done)))))
+
+(deftest waterfall-should-call-many-functions-in-sequence
+    (async/timeout 0 all-done
+        (extension.async/waterfall
+            (fn [done] (done 1))
+            (fn [arg done] (done (+ arg 1)))
+            (fn [arg done] (done (+ arg 1)))
+            (fn [arg done]
+                (is (= arg 3))
+                (done))
+            all-done)))
