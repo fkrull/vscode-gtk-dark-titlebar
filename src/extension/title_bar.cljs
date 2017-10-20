f61a3397191697900a0badecb66934da15e08788
Felix Krull <f_krull@gmx.de>
2017-10-20 22:19:34 +0200

    Parse more output.

--- src/extension/title_bar.cljs
+++ src/extension/title_bar.cljs
@@ -1,10 +1,14 @@
 (ns extension.title-bar
     (:require [extension.util :refer [parse-int]]))
 
-(defn update-title-bar! [])
+(defn update-title-bar! [mode]
+    ())
 
 (defn parse-pids [output]
     (map parse-int (re-seq #"\d+" output)))
 
 (defn parse-window-ids [output]
     (re-seq #"0x[\da-fA-F]+" output))
+
+(defn parse-window-pid [output]
+    (parse-int (re-find #"\d+" output)))
--- src/test/test_title_bar.cljs
+++ src/test/test_title_bar.cljs
@@ -11,3 +11,8 @@
     (is (=
          (extension.title-bar/parse-window-ids "_NET_CLIENT_LIST(WINDOW): window id # 0x260000a, 0x260000a")
          ["0x260000a" "0x260000a"])))
+
+(deftest should-get-pids-from-xprop-output
+    (is (=
+         (extension.title-bar/parse-window-pid "_NET_WM_PID(CARDINAL) = 1564")
+         1564)))
