3da9b15dced6c5a57faadb5a99062a8291f456c2
Felix Krull <f_krull@gmx.de>
2017-10-25 22:51:18 +0200

    Implemented theme variant code.

--- src/gtk-theme-variant.ts
+++ src/gtk-theme-variant.ts
@@ -5,26 +5,64 @@ export enum GtkThemeVariant {
     Dark,
 }
 
+function getVariantString(variant: GtkThemeVariant): string {
+    switch (variant) {
+        case GtkThemeVariant.Light: return 'light';
+        case GtkThemeVariant.Dark: return 'dark';
+    }
+}
+
+type ProcessOutput = typeof defaultProcessOutput;
+type ProcessID = number;
+type WindowID = string;
+
+interface PIDtoWID {
+    readonly pid: ProcessID;
+    readonly wid: WindowID;
+}
+
 function match(text: string, term: string | RegExp): string[] {
     const matches = text.match(term);
     return matches != null ? matches : [];
 }
 
+async function getEditorPIDs(processOutput: ProcessOutput): Promise<ProcessID[]> {
+    const output = await processOutput(['pidof', process.execPath]);
+    return match(output, /\d+/g)
+        .map((s) => parseInt(s, 10));
+}
+
+async function getWindowIDs(processOutput: ProcessOutput): Promise<WindowID[]> {
+    const output = await processOutput(['xprop', '-root', '_NET_CLIENT_LIST']);
+    return match(output, /0x[\da-fA-F]+/g);
+}
+
+async function getProcessIDsForWindowIDs(wids: WindowID[], processOutput: ProcessOutput): Promise<PIDtoWID[]> {
+    return await Promise.all(
+        wids.map(async (wid) => {
+            const output = await processOutput(['xprop', '-id', wid, '_NET_WM_PID']);
+            const pidString = match(output, /\d+/)[0] || '0';
+            return { wid, pid: parseInt(pidString, 10) };
+        }),
+    );
+}
+
+async function setThemeVariantOnWindow(
+        windowID: WindowID,
+        variant: GtkThemeVariant,
+        processOutput: ProcessOutput): Promise<void> {
+    await processOutput(['xprop', '-id', windowID,
+                         '-f', '_GTK_THEME_VARIANT', '8u',
+                         '-set', '_GTK_THEME_VARIANT', getVariantString(variant)]);
+}
+
 export async function setGtkThemeVariant(
         variant: GtkThemeVariant,
-        processOutput: typeof defaultProcessOutput = defaultProcessOutput): Promise<void> {
-    const pids = match(await processOutput(['pidof', process.execPath]), /\d+/g).map((s) => parseInt(s, 10));
-    const windowIds = match(await processOutput(['xprop', '-root', '_NET_CLIENT_LIST']), /0x[\da-fA-F]+/g);
-    const windowPids = await Promise.all(windowIds.map(async (wid): Promise<[string, number]> => {
-        const pid = match(await processOutput(['xprop', '-id', wid, '_NET_WM_PID']), /\d+/);
-        return [wid, parseInt(pid[0], 10)];
-    }));
+        processOutput: ProcessOutput = defaultProcessOutput): Promise<void> {
+    const editorPIDs = await getEditorPIDs(processOutput);
+    const allWindows = await getProcessIDsForWindowIDs(await getWindowIDs(processOutput), processOutput);
 
-    const setters: Array<Promise<string>> = [];
-    for (const [wid, pid] of windowPids) {
-        if (pids.includes(pid)) {
-            setters.push(processOutput(['xprop', '-id', wid, '-f', '_GTK_THEME_VARIANT', '8u', '-set', '_GTK_THEME_VARIANT', variant === GtkThemeVariant.Dark ? 'dark' : 'light']));
-        }
-    }
-    await Promise.all(setters);
+    await Promise.all(allWindows
+        .filter(({ wid, pid }) => editorPIDs.includes(pid))
+        .map(({ wid }) => setThemeVariantOnWindow(wid, variant, processOutput)));
 }
