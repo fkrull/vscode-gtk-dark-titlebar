202c36f0a749d788dc38ef01a2b375784d978d65
Felix Krull <f_krull@gmx.de>
2017-10-25 23:13:32 +0200

    Implement config setting.

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,25 @@ import * as vscode from 'vscode';
 import { GtkThemeVariant, setGtkThemeVariant } from './gtk-theme-variant';
 import processOutput from './process-output';
 
-export function activate(context: vscode.ExtensionContext) {
-    setGtkThemeVariant(GtkThemeVariant.Dark)
+function getThemeVariant(): GtkThemeVariant {
+    const config = vscode.workspace.getConfiguration('gtkTitleBar');
+    switch (config.get('mode')) {
+        case 'dark': return GtkThemeVariant.Dark;
+        case 'light': return GtkThemeVariant.Light;
+        default: return GtkThemeVariant.Light;
+    }
+}
+
+function updateGtkThemeVariant(): Promise<void> {
+    return setGtkThemeVariant(getThemeVariant())
         .catch((error) => {
             vscode.window.showErrorMessage(error.message);
         });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    updateGtkThemeVariant();
+    vscode.workspace.onDidChangeConfiguration(updateGtkThemeVariant);
 }
 
 export function deactivate() {
