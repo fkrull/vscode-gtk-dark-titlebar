558bbb8f2c60aaef2d992f7dcecdb08325e64679
Felix Krull <f_krull@gmx.de>
2017-10-25 22:59:04 +0200

    Ignore some things, add some error handling.

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,10 @@ import { GtkThemeVariant, setGtkThemeVariant } from './gtk-theme-variant';
 import processOutput from './process-output';
 
 export function activate(context: vscode.ExtensionContext) {
-    setGtkThemeVariant(GtkThemeVariant.Dark);
+    setGtkThemeVariant(GtkThemeVariant.Dark)
+        .catch((error) => {
+            vscode.window.showErrorMessage(error.message);
+        });
 }
 
 export function deactivate() {
